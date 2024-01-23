7bac3b1c96a1f6e69c2cd90d5ddf521d9edd2153
kongtaoxing <xyf8658@163.com>
2024-01-23 09:50:23 +0800

    migrate to sepolia

--- src/client.rs
+++ src/client.rs
@@ -3,27 +3,27 @@ use std::env;
 use dotenv::dotenv;
 use ethers::prelude::*;
 
-pub async fn get_goerli_client() -> Provider<Http> {
+pub async fn get_sepolia_client() -> Provider<Http> {
     dotenv().ok();
-    let goerli_url: String = env::var("GOERLI_URL").expect("GOERLI_URL must be set");
-    let rpc_url: &str = &goerli_url.as_str();
+    let sepolia_url: String = env::var("SEPOLIA_URL").expect("SEPOLIA_URL must be set");
+    let rpc_url: &str = &&sepolia_url.as_str();
     
     Provider::<Http>::try_from(rpc_url).unwrap()
 }
 
-pub async fn get_op_goerli_client() -> Provider<Http> {
+pub async fn get_op_sepolia_client() -> Provider<Http> {
     dotenv().ok();
-    let goerli_url: String = env::var("OPGOERLI_URL").expect("OPGOERLI_URL must be set");
-    let rpc_url: &str = &goerli_url.as_str();
-
+    let op_sepolia_url: String = env::var("OPSEPOLIA_URL").expect("OPSEPOLIA_URL must be set");
+    let rpc_url: &str = &&op_sepolia_url.as_str();
+    
     Provider::<Http>::try_from(rpc_url).unwrap()
 }
 
-pub async fn get_arb_goerli_client() -> Provider<Http> {
+pub async fn get_arb_sepolia_client() -> Provider<Http> {
     dotenv().ok();
-    let goerli_url: String = env::var("ARBGORLI_URL").expect("ARBGORLI_URL must be set");
-    let rpc_url: &str = &goerli_url.as_str();
-
+    let arb_sepolia_url: String = env::var("ARBSEPOLIA_URL").expect("ARBSEPOLIA_URL must be set");
+    let rpc_url: &str = &&arb_sepolia_url.as_str();
+    
     Provider::<Http>::try_from(rpc_url).unwrap()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let provider = client::get_goerli_client().await;
+    let provider = client::get_sepolia_client().await;
     let block_number: U64 = provider.get_block_number().await?;
     println!("Current Block Number is {block_number}");
 
